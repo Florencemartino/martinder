0e7843fe5045bcd2804fa7c332ae6cbdaae7ec92
F Martino <florence.martino@lifen.fr>
2021-01-18 23:51:20 +0100

    Ajout des vues edit/user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,26 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def update
-    @user = User.create!(user_params)
+    @user = User.find(params[:id])
+
+    @user.update!(user_params)
+
+    redirect_to user_path(@user.id)
   end
   
   
   private
   
   def user_params
-    params.require(:user).permit(:photos)
+    params.require(:user).permit(:photos, :first_name, :last_name, :nickname, :age, :sex_preference)
   end
 
 end
